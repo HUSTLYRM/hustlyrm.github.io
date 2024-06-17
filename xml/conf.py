b098386fdebf3ae4b70bf81b775b64ca0e5b0bc1
Sango <letusgoone@outlook.com>
2024-06-17 11:07:04 +0800

    为 sphinx 添加了 github extension

--- xml/conf.py
+++ xml/conf.py
@@ -4,7 +4,14 @@ copyright = '2024, '
 author = 'Sango'
 release = '1.0.2.0'
 version = '1.0.2.0'
-extensions = [ "breathe" ]
+
+extensions = [ 
+    'breathe',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
+]
+
 templates_path = ['_templates']
 exclude_patterns = []
 language = 'zh_CN'
